b0a0885c5e4b1c63d5e0014bce36bf5e0d0ceac9
ellie-rain <boudicealieb@gmail.com>
2021-08-06 17:22:22 +0300

    part1: 1.12.-1.14.

---
+++ part1/anecdotes/README.md
@@ -0,0 +1,17 @@
+#### 1.12\*: anecdotes step1
+
+The world of software engineering is filled with anecdotes that distill timeless truths from our field into short one-liners.
+
+Expand the application by adding a button that can be clicked to display a random anecdote from the field of software engineering.
+
+Content of the file index.js is same as in previous exercises.
+
+Find out how to generate random numbers in JavaScript, eg. via search engine or on Mozilla Developer Network. Remember that you can test generating random numbers e.g. straight in the console of your browser.
+
+#### 1.13\*: anecdotes step2
+
+Expand your application so that you can vote for the displayed anecdote.
+
+#### 1.14\*: anecdotes step3
+
+Now implement the final version of the application that displays the anecdote with the largest number of votes:
---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+const Anecdote = ({ anecdote, votes }) => (
+  <div>
+    <div>{anecdote}</div>
+    <div>has {votes} votes</div>
+  </div>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients',
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(() => new Array(anecdotes.length).fill(0))
+
+  const handleNextAnecdote = () => {
+    let randomId = selected
+
+    do {
+      randomId = Math.floor(Math.random() * anecdotes.length)
+    } while (randomId === selected)
+
+    setSelected(randomId)
+  }
+
+  const handleVote = () =>
+    setVotes((state) => {
+      const newState = [...state]
+      newState[selected] += 1
+      return newState
+    })
+
+  const mostVotedAnecdoteId = votes.indexOf(Math.max(...votes))
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <Anecdote anecdote={anecdotes[selected]} votes={votes[selected]} />
+
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleNextAnecdote}>next anecdote</button>
+
+      <h1>Anecdote with most votes</h1>
+      <Anecdote anecdote={anecdotes[mostVotedAnecdoteId]} votes={votes[mostVotedAnecdoteId]} />
+    </div>
+  )
+}
+
+export default App
